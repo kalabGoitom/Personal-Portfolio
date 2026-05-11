f36d954a7b58748d84cb34d9400ffed58cc9ea67
Kalab Goitom <kalabgoitommerese@gmail.com>
2026-05-11 15:16:26 +0300

    changed the skill status

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const techs = [
   {
     logo: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
     name: "React",
-    level: "Advanced",
+    level: "Beginner",
     color: "#61dafb",
   },
   // {
